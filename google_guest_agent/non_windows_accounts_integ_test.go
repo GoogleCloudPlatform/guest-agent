14b737dd39e1075b54f8f6fecc308950ad630bf7
Han Gao <hanga@google.com>
2021-09-08 18:29:53 +0000

    address comments

--- google_guest_agent/non_windows_accounts_integ_test.go
+++ google_guest_agent/non_windows_accounts_integ_test.go
@@ -8,42 +8,57 @@ import (
 	"testing"
 )
 
-const testUser = "integration-test-user"
+const (
+	testUser       = "integration-test-user"
+	expectedGroups = []string{"adm", "dip", "docker", "lxd", "plugdev", "video", "google-sudoers"}
+)
 
 func TestCreateGoogleUser(t *testing.T) {
+	if exist, err := userExists(testUser); exist == true {
+		t.Errorf("test user should not exist")
+	}
 	if err := createGoogleUser(testUser); err != nil {
 		t.Errorf("failed creating test user")
 	}
 	if exist, err := userExists(testUser); exist != true || err != nil {
-		t.Errorf("tesr user %s should exist", testUser)
+		t.Errorf("tesr user should exist")
 	}
 	cmd := exec.Command("groups", testUser)
-	out := runCmdOutput(cmd).Stdout()
-	groups := strings.Split(strings.TrimSpace(strings.Split(out, ":")[1]), " ")
+	ret := runCmdOutput(cmd)
+	if ret.ExitCode() != 0 {
+		t.Errorf("test user should be added to group")
+	}
+	groups := strings.Split(strings.TrimSpace(strings.Split(ret.Stdout(), ":")[1]), " ")
 	for _, group := range groups {
-		if !strings.Contains("adm,dip,docker,lxd,plugdev,video,google-sudoers", group) {
-			t.Errorf("test user has been added to an unexpected group")
+		if !contains(group, expectedGroups) {
+			t.Errorf("test user has been added to an unexpected group %s", group)
+		}
+	}
+	for _, expected := range expectedGroups {
+		if !contains(expected, groups) {
+			t.Errorf("test user has not been added to group %s", expected)
 		}
 	}
 	if err := createGoogleUser(testUser); err == nil {
-		t.Errorf("expected user exist and return error but not")
+		t.Errorf("user should exist and return error but not")
 	}
 }
 
 func TestRemoveGoogleUser(t *testing.T) {
+	if exist, err := userExists(testUser); exist == true {
+		t.Errorf("test user should not exist")
+	}
 	if err := createGoogleUser(testUser); err != nil {
 		t.Errorf("failed creating test user")
 	}
 	if err := removeGoogleUser(testUser); err != nil {
 		t.Errorf("failed when remove google user")
 	}
-	cmd := exec.Command("groups", testUser)
-	ret := runCmdOutput(cmd)
-	if ret.ExitCode() != 1 {
-		t.Errorf("expected groups command return error code 1, but actually error is %d, %s", ret.ExitCode(), ret.Stderr())
+	if exist, err := userExists(testUser); exist == true {
+		t.Errorf("test user should not exist")
 	}
 	if err := removeGoogleUser(testUser); err == nil {
-		t.Errorf("expected user has been removed and return error but not")
+		t.Errorf("user has been removed and should return error but not")
 	}
 }
 
@@ -58,4 +73,13 @@ func TestGroupaddDuplicates(t *testing.T) {
 	if ret.ExitCode() != 9 {
 		t.Fatalf("got wrong exit code running \"groupadd integ-test-group\", expected 9 got %v\n", ret.ExitCode())
 	}
+}
+
+func contains(target string, expected []string) bool {
+	for _, e := range expected {
+		if e == target {
+			return true
+		}
+	}
+	return false
 }
