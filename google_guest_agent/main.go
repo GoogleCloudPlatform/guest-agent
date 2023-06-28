00c2315d59740ada13bcfcf082b1d3959fa0b248
Ozerov Andrei <ozerovandrei@yandex-team.ru>
2023-06-28 10:46:44 +0000

    Pull request #3: CLOUD-141399: Disable cloud logging
    Merge in CLOUD/mk8s-guest-agent from feature/CLOUD-141399/20230601.00-mk8s-disable-cloud-logging to 20230601.00-mk8s
    
    Squashed commit of the following:
    
    commit 29891b8e58690a3d740693f7c5e1f3bbd488e83d
    Author: Andrei Ozerov <ozerovandrei@yandex-team.ru>
    Date:   Wed Jun 21 14:09:53 2023 +0200
    
        CLOUD-141399: Disable cloud logging

--- google_guest_agent/main.go
+++ google_guest_agent/main.go
@@ -116,7 +116,10 @@ func runUpdate() {
 }
 
 func run(ctx context.Context) {
-	opts := logger.LogOpts{LoggerName: programName}
+	opts := logger.LogOpts{
+		LoggerName:          programName,
+		DisableCloudLogging: true,
+	}
 	if runtime.GOOS == "windows" {
 		opts.FormatFunction = logFormatWindows
 		opts.Writers = []io.Writer{&utils.SerialPort{Port: "COM1"}}
