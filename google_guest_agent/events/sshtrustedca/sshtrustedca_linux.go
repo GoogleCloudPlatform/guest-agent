3e4b458205883f3ab20c35ddbff3cfa8c448ca5f
Leandro Dorileo <dorileo@google.com>
2023-07-20 13:32:25 -0700

    sshTrustedCA: add pipe event handler (#222)
    The integration between sshd and metadata uses a named pipe, that
    gives us the flexibility to read the certificate data from metadata on
    demand.
    
    The implementation lays on top of the events layer, when a open
    operation is performed on the pipe the sshTrustedCA watcher will
    open as write only, notify its subscribers of the event(passing down
    the open file), the subscriber is responsible to close the file. If
    multiple subscribers exist their implementation should coordinate when
    and how to close the pipe.

---
+++ google_guest_agent/events/sshtrustedca/sshtrustedca_linux.go
@@ -0,0 +1,99 @@
+//  Copyright 2023 Google Inc. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package sshtrustedca
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+
+	"github.com/GoogleCloudPlatform/guest-logging-go/logger"
+)
+
+// Create a named pipe if it doesn't exist.
+func createNamedPipe(pipePath string) error {
+	pipeDir := filepath.Dir(pipePath)
+	_, err := os.Stat(pipeDir)
+
+	if err != nil && os.IsNotExist(err) {
+		// The perm 0755 is compatible with distros /etc/ssh/ directory.
+		if err := os.MkdirAll(pipeDir, 0755); err != nil {
+			return err
+		}
+	}
+
+	if _, err := os.Stat(pipePath); err != nil {
+		if os.IsNotExist(err) {
+			if err := syscall.Mkfifo(pipePath, 0644); err != nil {
+				return fmt.Errorf("failed to create named pipe: %+v", err)
+			}
+		} else {
+			return fmt.Errorf("failed to stat file: " + pipePath)
+		}
+	}
+	return nil
+}
+
+// Run listens to ssh_trusted_ca's pipe open calls and report back the event.
+func (mp *Watcher) Run(ctx context.Context) (bool, string, interface{}, error) {
+	var canceled bool
+
+	// Cancelation handling code.
+	go func() {
+		select {
+		case <-ctx.Done():
+			canceled = true
+
+			// Open the pipe as O_RDONLY to release the blocking open O_WRONLY.
+			pipeFile, err := os.OpenFile(mp.pipePath, os.O_RDONLY, 0644)
+			if err != nil {
+				logger.Errorf("Failed to open readonly pipe: %+v", err)
+				return
+			}
+
+			defer func() {
+				if err := pipeFile.Close(); err != nil {
+					logger.Errorf("Failed to close readonly pipe: %+v", err)
+				}
+			}()
+		}
+	}()
+
+	// If the configured named pipe doesn't exists we create it before emitting events
+	// from it.
+	if err := createNamedPipe(mp.pipePath); err != nil {
+		return true, ReadEvent, nil, err
+	}
+
+	// Open the pipe as writeonly, it will block until a read is performed from the
+	// other end of the pipe.
+	pipeFile, err := os.OpenFile(mp.pipePath, os.O_WRONLY, 0644)
+	if err != nil {
+		return true, ReadEvent, nil, err
+	}
+
+	// Have we got a ctx.Done()? if so lets just return from here and unregister
+	// the watcher.
+	if canceled {
+		if err := pipeFile.Close(); err != nil {
+			logger.Errorf("Failed to close readonly pipe: %+v", err)
+		}
+		return false, ReadEvent, nil, nil
+	}
+
+	return true, ReadEvent, &PipeData{File: pipeFile}, nil
+}
