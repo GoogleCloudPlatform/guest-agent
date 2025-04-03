5908d263df2e35af0c3138a920c1a26a2daa2c97
Chaitanya Kulkarni <chaitanyauk@google.com>
2025-04-03 23:09:45 +0000

    Skip packaging legacy guest agent

--- packaging/googet/windows_agent_build.sh
+++ packaging/googet/windows_agent_build.sh
@@ -20,9 +20,7 @@
 
 version=$1
 
-GOOS=windows /tmp/go/bin/go build -ldflags "-X main.version=$version" -mod=readonly -o GCEWindowsAgent.exe ./google_guest_agent
 GOOS=windows /tmp/go/bin/go build -ldflags "-X main.version=$version" -mod=readonly -o GCEAuthorizedKeysCommand.exe ./google_authorized_keys
-
 
 # Script expects guest-agent and google-guest-agent codebase are placed
 # side-by-side within same directory and this script is executed from root of 
