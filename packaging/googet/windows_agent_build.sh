28f0ad0e2d783484692fd2d28ff41b9826d21dc9
Andrew Li <65982444+drewhli@users.noreply.github.com>
2026-02-13 18:03:16 -0800

    Add packaging changes for locally bundled extensions feature support (#593)

--- packaging/googet/windows_agent_build.sh
+++ packaging/googet/windows_agent_build.sh
@@ -40,6 +40,7 @@ if [[ ! -f "$GUEST_AGENT_REPO/Makefile" ]]; then
     echo "This is a placeholder file so guest agent package build without error. Package will have actual Guest Agent Manager executable instead if both repos are cloned side-by-side." > GCECompatMetadataScripts.exe
     echo "This is a placeholder file so guest agent package build without error. Package will have actual Guest Agent Manager executable instead if both repos are cloned side-by-side." > GCEAuthorizedKeys.exe
     echo "This is a placeholder file so guest agent package build without error. Package will have actual Guest Agent Manager executable instead if both repos are cloned side-by-side." > GCEAuthorizedKeysNew.exe
+    echo "This is a placeholder file so guest agent package build without error. Package will have actual Guest Agent Manager executable instead if both repos are cloned side-by-side." > core_plugin.manifest.binpb
     exit 0
 fi
 
@@ -60,6 +61,7 @@ cp cmd/google_guest_compat_manager/google_guest_compat_manager $BUILD_DIR/GCEWin
 cp cmd/core_plugin/core_plugin $BUILD_DIR/CorePlugin.exe
 cp cmd/gce_metadata_script_runner/gce_metadata_script_runner $BUILD_DIR/GCEMetadataScriptRunner.exe
 cp cmd/metadata_script_runner_compat/gce_compat_metadata_script_runner $BUILD_DIR/GCECompatMetadataScripts.exe
+cp build/configs/usr/lib/google/guest_agent/GuestAgentCorePlugin/manifest.windows.binpb $BUILD_DIR/core_plugin.manifest.binpb
 
 if [[ -f cmd/google_authorized_keys_compat/google_authorized_keys_compat ]]; then
   cp $BUILD_DIR/GCEAuthorizedKeysCommand.exe $BUILD_DIR/GCEAuthorizedKeys.exe
