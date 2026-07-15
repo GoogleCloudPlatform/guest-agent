3852c03a09c6eb8fb44e0ea3bdd5d7677c5bf87a
Yage Hu <yagehu@google.com>
2026-07-15 15:23:19 -0700

    Support basic account management on FreeBSD (#619)
    This commit ports the basic account management feature of the Guest
    Agent to FreeBSD. This includes:
    
    - Automatic account and group creation
    - Initial SSH public key provisioning
    - Adding user to sudoers file
    
    However, this does not include restarting sshd. This is because it's
    currently only best-effort on Linux and FreeBSD does not use systemd. I
    plan on adding logic to restart sshd properly on FreeBSD later.
    
    With this change, it is now possible to SSH into a FreeBSD VM with a
    static SSH key provided on boot.

---
+++ google_guest_agent/cfg/cfg_freebsd.go
@@ -0,0 +1,12 @@
+package cfg
+
+const (
+	// InstallPathPrefix is /usr/local for all third-party
+	// software in FreeBSD.
+	InstallPathPrefix = "/usr/local"
+
+	// DataPathPrefix is where applications store their persistent,
+	// dynamic data.
+	// In FreeBSD, /var/db is designated for this purpose.
+	DataPathPrefix = "/var/db"
+)
