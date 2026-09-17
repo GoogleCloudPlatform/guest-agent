dffb601ac83a693b16be53fb85a52c92232e95b4
Andrew Li <andrewhl@google.com>
2026-09-17 11:34:16 -0700

    Validate username using unicode.IsSpace to reject all Unicode whitespace characters, preventing privilege escalation via Unicode space injection

--- utils/ssh_test.go
+++ utils/ssh_test.go
@@ -117,6 +117,11 @@ func TestValidateUser(t *testing.T) {
 		{"user\t-g", false},
 		{"user\n-g", false},
 		{"username\t-g\n27", false},
+		{"user\u00a0-g", false},
+		{"user\u2000-g", false},
+		{"user\u2009-g", false},
+		{"user\u202f-g", false},
+		{"user\u3000-g", false},
 	}
 	for _, tt := range table {
 		err := ValidateUser(tt.user)
